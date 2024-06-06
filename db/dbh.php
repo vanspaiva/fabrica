25efef9d731188bb780d2c270674638a9bf3d86f
brunacoelhofix <bruna.coelho@fixhealth.com.br>
2024-06-06 09:25:30 -0300

    feat: bd produtos e correlacao_produtos

--- db/dbh.php
+++ db/dbh.php
@@ -1,14 +1,15 @@
 <?php
 
-$serverName = "localhost";
-$dbUsername = "u777557116_adminfabrica";
-$dbPassword = "CcIB8WQT36a";
-$dbName = "u777557116_bdcpmhfabrica";
+//$serverName = "localhost";
+//$dbUsername = "u777557116_adminfabrica";
+//$dbPassword = "CcIB8WQT36a";
+//$dbName = "u777557116_bdcpmhfabrica";
 
-// $serverName = "localhost";
-// $dbUsername = "root";
-// $dbPassword = "";
-// $dbName = "fabrica";
+
+$serverName = "localhost";
+$dbUsername = "root";
+$dbPassword = "";
+$dbName = "fabrica";
 
 
 $conn = mysqli_connect($serverName, $dbUsername, $dbPassword, $dbName);
