80951d0e62c546babc52a307065d9899db325628
brunacoelhofix <bruna.coelho@fixhealth.com.br>
2024-07-09 11:31:00 -0300

    adicionando a dash do frm_inf_004. pois a outra for perdida

--- dashboard/dash.view.php
+++ dashboard/dash.view.php
@@ -178,6 +178,29 @@
                                 </div>
                             </div>
                         </div>
+
+                         <!-- Módulo Manutenção e Limpeza Ar-condicionado -->
+                         <div class="col-sm my-2">
+                            <div class="card border-left-primary shadow h-100 py-2">
+                                <div class="card-body">
+                                    <div class="row no-gutters align-items-center">
+                                        <h6 class=""><b>Módulo Manutenção e Limpeza Ar-condicionado</b></h6>
+                                    </div>
+                                    <hr>
+                                    <div class="row d-flex justify-content-center my-1 py-1">
+                                        <div class="d-flex justify-content-between px-2">
+                                            <a href="frm_inf_004" class="btn btn-fab mx-1"><i class="fas fa-plus"></i> Novo ML</a>
+                                            <a href="lista-frm" class="btn btn-outline-fab mx-1" style="border-top: 6px #3d7869 solid;"> <i class="fas fa-list"></i> Lista de Registros</a>
+                                        </div>
+                                    </div>
+                                    <div class="row d-flex justify-content-center my-1 py-1">
+                                        <div class="d-flex justify-content-between px-2">
+                                            <a href="pendencia_frm" class="btn btn-outline-fab mx-1" style="border-top: 6px #3d7869 solid;"><i class="fas fa-users-cog"></i> Registros Pendentes</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 <?php
                 }
