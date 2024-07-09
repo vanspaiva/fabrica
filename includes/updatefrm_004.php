ca62f04813cfa9a756cf389ce833c8a547052e72
brunacoelhofix <bruna.coelho@fixhealth.com.br>
2024-07-09 08:03:21 -0300

    atualizando as informaçoes inseridas

--- includes/updatefrm_004.php
+++ includes/updatefrm_004.php
@@ -7,9 +7,11 @@ if (isset($_POST['update'])) {
     $dataPublicacao = $_POST['dataPublicacao'];
     $dataValidade = $_POST['dataValidade'];
     $dataManutencao = $_POST['dataManutencao'];
-    $modelo = $_POST['marcaModelo'];
-    $descricaoSetor = $_POST['setor_id'];
+    $modelo = $_POST['marcaModelo'];   
+    $setorId = $_POST['setor_id'];
     $descricaoAtividadesIds = isset($_POST['descricao_atividades']) ? $_POST['descricao_atividades'] : [];
+
+    $frmStatus = 1; 
 
     if ($conn->connect_error) {
         die("Conexão falhou: " . $conn->connect_error);
@@ -18,6 +20,37 @@ if (isset($_POST['update'])) {
     $conn->begin_transaction();
 
     try {
+        // 1. Deletar os registros antigos na tabela frm_inf_004_atividades
+        $sqlDelete = "DELETE FROM frm_inf_004_atividades WHERE frm_inf_004_id = ?";
+        $stmtDelete = $conn->prepare($sqlDelete);
+        $stmtDelete->bind_param('i', $frmId);
+        $stmtDelete->execute();
+
+        // 2. Inserir os novos IDs selecionados na tabela frm_inf_004_atividades
+        $sqlInsert = "INSERT INTO frm_inf_004_atividades (frm_inf_004_id, descricao_atividades_id) VALUES (?, ?)";
+        $stmtInsert = $conn->prepare($sqlInsert);
+
+        foreach ($descricaoAtividadesIds as $atividadeId) {
+            $stmtInsert->bind_param('ii', $frmId, $atividadeId);
+            $stmtInsert->execute();
+        }
+
+        // 3. Atualizar os dados na tabela frm_inf_004
+        // Obter a descrição do setor
+        $sqlSetor = "SELECT descricao_setores FROM setor_arcondicionado WHERE id = ?";
+        $stmtSetor = $conn->prepare($sqlSetor);
+        $stmtSetor->bind_param('i', $setorId);
+        $stmtSetor->execute();
+        $resultSetor = $stmtSetor->get_result();
+
+        if ($resultSetor->num_rows > 0) {
+            $descricao_setor = $resultSetor->fetch_assoc()['descricao_setores'];
+        } else {
+            throw new Exception('Setor não encontrado.');
+        }
+        $stmtSetor->close();
+
+        // Montar a descrição das atividades selecionadas
         $descricaoAtividades = "";
         if (!empty($descricaoAtividadesIds)) {
             $atividadesNomes = [];
@@ -36,26 +69,18 @@ if (isset($_POST['update'])) {
             $descricaoAtividades = implode(", ", $atividadesNomes);
         }
 
-        $frmStatus = ($_POST['frmStatus'] == 'Pendente') ? 1 : 2;
-
+        // Atualizar os dados na tabela frm_inf_004
         $sqlUpdate = "UPDATE frm_inf_004 SET 
                       data_publicacao = STR_TO_DATE(?, '%Y-%m-%d'), 
                       data_validade = STR_TO_DATE(?, '%Y-%m-%d'), 
-                      modelo = ?, 
                       data_manutencao = STR_TO_DATE(?, '%Y-%m-%d'), 
-                      descricao_atividades = ?, 
+                      modelo = ?, 
                       descricao_setor = ?, 
+                      descricao_atividades = ?, 
                       frmstatus_id = ? 
                       WHERE id = ?";
         $stmtUpdate = $conn->prepare($sqlUpdate);
-
-        if (!$stmtUpdate) {
-            throw new Exception('Erro na preparação da consulta SQL para atualização: ' . $conn->error);
-        }
-
-
-        $stmtUpdate->bind_param('ssssssii', $dataPublicacao, $dataValidade, $modelo, $dataManutencao, $descricaoAtividades, $descricaoSetor, $frmStatus, $frmId);
-
+        $stmtUpdate->bind_param('ssssssii', $dataPublicacao, $dataValidade, $dataManutencao, $modelo, $descricao_setor, $descricaoAtividades, $frmStatus, $frmId);
 
         if (!$stmtUpdate->execute()) {
             throw new Exception('Erro ao executar a consulta SQL para atualização: ' . $stmtUpdate->error);
