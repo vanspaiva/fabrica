89a0136db155b4ed346b8b8d1d949b386d7472d3
brunacoelhofix <bruna.coelho@fixhealth.com.br>
2024-07-09 08:02:54 -0300

    melhorando a legibilidade do campo

--- dashboard/dash.view.php
+++ dashboard/dash.view.php
@@ -96,23 +96,14 @@
                         <div class="card border-left-primary shadow h-100 py-2" disabled>
                             <div class="card-body">
                                 <div class="row no-gutters align-items-center">
-                                    <h6 class="deactivated"><b>Formulário FRM.INF.004</b></h6>
+                                    <h6 class="deactivated"><b>Formulário Manutenção e Limpeza Ar-Condicionado</b></h6>
                                 </div>
                                 <hr>
                                 <div class="row d-flex justify-content-center my-1 py-1">
                                     <div class="d-flex justify-content-between px-2">
                                         <a href="frm_inf_004" class="btn btn-fab mx-1"><i class="fas fa-plus"></i> Novo form </a>
                                         <a href="lista-frm" class="btn btn-outline-fab mx-1" style="border-top: 6px #007A5A solid;"> <i class="fas fa-list"></i> Lista do FRM.004</a>
-                                        <a href="atividades" class="btn btn-outline-fab mx-1" style="border-top: 6px #007A5A solid;"> <i class="fas fa-thumbtack"></i> Quadro de Atividades</a>
                                     </div>
-                                </div>
-                                <!-- <div class="row d-flex justify-content-center my-1 py-1">
-                                    <div class="d-flex justify-content-between px-2">
-                                        <a href="pcp" class="btn btn-outline-info mx-1 disabled"><i class="fas fa-users-cog"></i> PCP </a>
-                                        <a href="opplanejamento" class="btn btn-outline-info mx-1 disabled"> <i class="fas fa-th-list"></i> Planejamento da Produção</a>
-                                        <a href="opetapas" class="btn btn-outline-info mx-1 disabled"> <i class="fas fa-th-large"></i> Etapas</a>
-                                    </div>
-                                </div> -->
                             </div>
                         </div>
                     </div>
