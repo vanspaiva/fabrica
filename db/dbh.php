cc3a17eb46c40824a9bbc57ad8114a5c06113d3c
CPMH Digital <153092588+vanspaiva@users.noreply.github.com>
2024-07-25 08:23:02 -0300

    Update dbh.php

--- db/dbh.php
+++ db/dbh.php
@@ -1,15 +1,15 @@
 <?php
 
-/* $serverName = "localhost";
+$serverName = "localhost";
 $dbUsername = "u777557116_adminfab";
 $dbPassword = "CcIB8WQT36a";
-$dbName = "u777557116_fabrica"; */
+$dbName = "u777557116_fabrica";
  
-$serverName = "localhost:3307";
+/* $serverName = "localhost:3307";
 $dbUsername = "root";
 $dbPassword = "";
 $dbName = "fabrica";
-
+ */
 
 $conn = mysqli_connect($serverName, $dbUsername, $dbPassword, $dbName);
 // $mysqli = new mysqli($serverName, $dbUsername, $dbPassword, $dbName) or die(mysqli_error($mysqli));
