e4b1fb3212fd3a10288c791eef4bbe30b3281da9
brunacoelhofix <bruna.coelho@fixhealth.com.br>
2024-07-29 10:33:18 -0300

    dash para serviços gerais

---
+++ dashboard/botoes_servicosGerais.php
@@ -0,0 +1,47 @@
+</div>
+<div class="row mb-4">
+<div class="col-md-6 my-2">
+    <div class="card border-left-primary shadow h-100 py-2">
+        <div class="card-body">
+            <div class="row no-gutters align-items-center">
+                <h6 class=""><b>Módulo Registros de Limpeza</b></h6>
+            </div>
+            <hr>
+            <div class="row d-flex justify-content-center my-1 py-1">
+                <div class="d-flex justify-content-between px-2">
+                    <a href="novoRegistro003" class="btn btn-info mx-1"><i class="fas fa-plus"></i> Nova RL </a>
+                    <a href="showForm003.php" class="btn btn-outline-info mx-1" style="border-top: 6px #129aaf solid;"> <i class="fas fa-list"></i> Lista de Registros</a>
+                </div>
+            </div>
+            <div class="row d-flex justify-content-center my-1 py-1">
+                <div class="d-flex justify-content-between px-2">
+                    <a href="cameraQRcode.php" class="btn btn-outline-dark mx-1"><i class="bi bi-qr-code-scan"></i></i> QR Code</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!--   Registro Manutenção e Limpeza Ar-condicionado  -->
+<div class="col-md-6 my-2">
+    <div class="card border-left-primary shadow h-100 py-2">
+        <div class="card-body">
+            <div class="row no-gutters align-items-center">
+                <h6 class=""><b>Registro Manutenção e Limpeza Ar-condicionado</b></h6>
+            </div>
+            <hr>
+            <div class="row d-flex justify-content-center my-1 py-1">
+                <div class="d-flex justify-content-between px-2">
+                    <a href="frm_inf_004" class="btn btn-success mx-1"><i class="fas fa-plus"></i> Novo RA</a>
+                    <a href="lista-frm" class="btn btn-outline-success mx-1" style="border-top: 6px #28a745  solid;"> <i class="fas fa-list"></i> Lista de Registros</a>
+                </div>
+            </div>
+            <div class="row d-flex justify-content-center my-1 py-1">
+                <div class="d-flex justify-content-between px-2">
+                    <a href="cameraQRcode.php" class="btn btn-outline-dark mx-1"><i class="bi bi-qr-code-scan"></i></i> QR Code</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
